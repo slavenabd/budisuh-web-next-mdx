8703a134cc9fae88bfc38f70fad7c75c11a8a7a9
Slaven Abdovic <35407666+slavenabd@users.noreply.github.com>
2025-11-04 22:31:05 +0100

    solving redirect loop

--- next.config.js
+++ next.config.js
@@ -4,75 +4,62 @@ const { withContentlayer } = require("next-contentlayer");
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  async redirects() {
+    return [
+      // Redirect www → apex (avoid duplicate content)
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.budisuh.eu" }],
+        destination: "https://budisuh.eu/:path*",
+        permanent: true,
+      },
+
+      // Old WordPress slugs → /blog/...
+      {
+        source: "/dijete-i-dalje-mokri-u-krevet-a-probali-smo-sve",
+        destination: "/blog/dijete-i-dalje-mokri-u-krevet-a-probali-smo-sve",
+        permanent: true,
+      },
+      {
+        source: "/dnevnik-mokrenja-pijenja-ispunjavanje-tumacenje",
+        destination: "/blog/dnevnik-mokrenja-pijenja-ispunjavanje-tumacenje",
+        permanent: true,
+      },
+      {
+        source: "/enureza-nocno-mokrenje-ukratko",
+        destination: "/blog/enureza-nocno-mokrenje-ukratko",
+        permanent: true,
+      },
+      {
+        source: "/nocno-mokrenje-enureza-djeca-dijagnostika-lijecenje",
+        destination:
+          "/blog/nocno-mokrenje-enureza-djeca-dijagnostika-lijecenje",
+        permanent: true,
+      },
+      {
+        source: "/pretrage-za-nocno-mokrenje-kod-djece",
+        destination: "/blog/pretrage-za-nocno-mokrenje-kod-djece",
+        permanent: true,
+      },
+      {
+        source: "/rezim-pijenja-i-mokrenja-prvi-korak-do-suhoce",
+        destination: "/blog/rezim-pijenja-i-mokrenja-prvi-korak-do-suhoce",
+        permanent: true,
+      },
+      {
+        source: "/rijetki-uzroci-nocnog-mokrenja",
+        destination: "/blog/rijetki-uzroci-nocnog-mokrenja",
+        permanent: true,
+      },
+      {
+        source: "/uspjesno-lijecenje-nocnog-mokrenja",
+        destination: "/blog/uspjesno-lijecenje-nocnog-mokrenja",
+        permanent: true,
+      },
+    ];
+  },
 };
 
+// Export correctly (only once!)
 module.exports = withContentlayer(nextConfig);
-
-module.exports = {
-  async redirects() {
-    return [
-      // www → apex (keep one canonical host)
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.budisuh.eu' }],
-        destination: 'https://budisuh.eu/:path*',
-        permanent: true,
-      },
-
-      // Explicit legacy WP → new blog slugs
-      {
-        source: '/dijete-i-dalje-mokri-u-krevet-a-probali-smo-sve',
-        destination: '/blog/dijete-i-dalje-mokri-u-krevet-a-probali-smo-sve',
-        permanent: true,
-      },
-      {
-        source: '/dnevnik-mokrenja-pijenja-ispunjavanje-tumacenje',
-        destination: '/blog/dnevnik-mokrenja-pijenja-ispunjavanje-tumacenje',
-        permanent: true,
-      },
-      {
-        source: '/enureza-nocno-mokrenje-ukratko',
-        destination: '/blog/enureza-nocno-mokrenje-ukratko',
-        permanent: true,
-      },
-      {
-        source: '/nocno-mokrenje-enureza-djeca-dijagnostika-lijecenje',
-        destination: '/blog/nocno-mokrenje-enureza-djeca-dijagnostika-lijecenje',
-        permanent: true,
-      },
-      {
-        source: '/pretrage-za-nocno-mokrenje-kod-djece',
-        destination: '/blog/pretrage-za-nocno-mokrenje-kod-djece',
-        permanent: true,
-      },
-      {
-        source: '/rezim-pijenja-i-mokrenja-prvi-korak-do-suhoce',
-        destination: '/blog/rezim-pijenja-i-mokrenja-prvi-korak-do-suhoce',
-        permanent: true,
-      },
-      {
-        source: '/rijetki-uzroci-nocnog-mokrenja',
-        destination: '/blog/rijetki-uzroci-nocnog-mokrenja',
-        permanent: true,
-      },
-      {
-        source: '/uspjesno-lijecenje-nocnog-mokrenja',
-        destination: '/blog/uspjesno-lijecenje-nocnog-mokrenja',
-        permanent: true,
-      },
-    ];
-  },
-};
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.budisuh.eu' }],
-        destination: 'https://budisuh.eu/:path*',
-        permanent: true,
-      },
-    ];
-  },
-};
